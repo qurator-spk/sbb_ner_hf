172eea3020a7fbd8729339b104a3344659b1f1e4
Kai Labusch <Kai.Labusch@sbb.spk-berlin.de>
2025-04-15 09:42:43 +0200

    multi-experiment script

---
+++ experiment.py
@@ -0,0 +1,131 @@
+import pandas as pd
+
+import config
+import train
+import eval_opt
+import click
+import os
+import pandas as pd
+
+models = [
+    {"path": "dbmdz/bert-mini-historic-multilingual-cased", "add_prefix_space": False},
+    {"path": "dbmdz/bert-base-german-cased", "add_prefix_space": False},
+    {"path": "FacebookAI/roberta-base", "add_prefix_space": True},
+    {"path": "dbmdz/bert-base-historic-multilingual-cased", "add_prefix_space": False},
+    {"path": "flair/ner-german", "add_prefix_space": False},
+    {"path": "distilbert/distilbert-base-uncased", "add_prefix_space": False}
+]
+
+datasets = [
+    {"name": "zefys2025", "path": "data/zefys2025_20250404.hf", "source": "local"},
+    {"name": "newseye", "path": "data/newseye_20250404.hf", "source": "local"},
+    {"name": "hisgerman", "path": "data/hisgermaner_20250404.hf", "source": "local"},
+    {"name": "conll2003", "path": "eriktks/conll2003", "source": "hf"},
+    {"name": "GermEval",  "path": "GermanEval/germeval_14", "source": "hf"}
+]
+
+
+def process_report(best_result, class_report):
+    total_support = 0.0
+    best_result['f1_test'] = 0.0
+    best_result['precision_test'] = 0.0
+    best_result['recall_test'] = 0.0
+
+    if 'PER' in class_report:
+        total_support += class_report['PER']['support']
+    if 'LOC' in class_report:
+        total_support += class_report['LOC']['support']
+    if 'ORG' in class_report:
+        total_support += class_report['ORG']['support']
+
+    if 'PER' in class_report:
+        weight = class_report['PER']['support'] / total_support
+        best_result['f1_test'] += weight * class_report['PER']['f1-score']
+        best_result['precision_test'] += weight * class_report['PER']['precision']
+        best_result['recall_test'] += weight * class_report['PER']['recall']
+
+        best_result['PER_f1_test'] = class_report['PER']['f1-score']
+        best_result['PER_precision_test'] = class_report['PER']['precision']
+        best_result['PER_recall_test'] = class_report['PER']['recall']
+
+    if 'LOC' in class_report:
+        weight = class_report['LOC']['support'] / total_support
+        best_result['f1_test'] += weight * class_report['LOC']['f1-score']
+        best_result['precision_test'] += weight * class_report['LOC']['precision']
+        best_result['recall_test'] += weight * class_report['LOC']['recall']
+
+        best_result['LOC_f1_test'] = class_report['LOC']['f1-score']
+        best_result['LOC_precision_test'] = class_report['LOC']['precision']
+        best_result['LOC_recall_test'] = class_report['LOC']['recall']
+
+    if 'ORG' in class_report:
+        weight = class_report['ORG']['support'] / total_support
+        best_result['f1_test'] += weight * class_report['ORG']['f1-score']
+        best_result['precision_test'] += weight * class_report['ORG']['precision']
+        best_result['recall_test'] += weight * class_report['ORG']['recall']
+
+        best_result['ORG_f1_test'] = class_report['ORG']['f1-score']
+        best_result['ORG_precision_test'] = class_report['ORG']['precision']
+        best_result['ORG_recall_test'] = class_report['ORG']['recall']
+
+    return best_result
+
+
+@click.command()
+@click.argument('result-file', type=click.Path())
+def main(result_file):
+
+    results = None
+    if os.path.exists(result_file):
+        results = pd.read_pickle(result_file)
+
+    for model_def, dataset_def in zip(models, datasets):
+
+        model_config = config.Resource(path=model_def["path"], source="hf")
+        model_config.set_name()
+        print(model_config.info())
+
+        dataset_config = config.Resource(path=dataset_def["path"], source=dataset_def["source"])
+        dataset_config.set_name()
+
+        print(dataset_config.info())
+
+        train.set_torch_device()
+
+        ner_dataset = train.load_ner_dataset(dataset_config.path, dataset_config.source)
+
+        tokenizer = train.get_tokenizer(model_def["path"], model_def["add_prefix_space"])
+
+        tokenized_dataset = train.prepare_dataset(ner_dataset, tokenizer)
+
+        label_list = train.get_label_list(ner_dataset)
+
+        training_params = config.TrainingParams()
+
+        training_params.batch_size = 32
+        training_params.num_train_epochs = 1
+
+        trained_ner_model, model_out_path, best_result = (
+            train.train_model(model_config, dataset_config, label_list, training_params,
+                              tokenized_dataset, tokenizer, save_strategy="epoch"))
+
+        class_report, errors = eval_opt.compute_metrics_per_tag(trained_ner_model, tokenized_dataset, label_list,
+                                                                output_dict=True)
+
+        result = process_report(best_result, class_report)
+
+        if results is None:
+            results = pd.DataFrame([result])
+        else:
+            results = pd.concat([results, pd.DataFrame([result])], axis=1)
+
+        # import ipdb;ipdb.set_trace()
+
+        if len(results) > 2:
+            break
+
+    results.to_pickle(result_file)
+
+
+if __name__ == '__main__':
+    main()
